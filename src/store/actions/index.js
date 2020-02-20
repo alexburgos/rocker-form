7e4f09301052ad664fd158ac33c804413ca4f2fa
Alex Burgos <alexander.burgos1@gmail.com>
2020-02-20 13:15:26 +0100

    changing let to const in some cases

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -100,8 +100,8 @@ export function cacheFormState() {
 
 export function fetchCountries() {
 	return async dispatch => {
-		let response = await fetch('https://restcountries.eu/rest/v2/all');
-		let countries = await response.json();
+		const response = await fetch('https://restcountries.eu/rest/v2/all');
+		const countries = await response.json();
 
 		localStorage.setItem('countries', JSON.stringify(countries));
 
